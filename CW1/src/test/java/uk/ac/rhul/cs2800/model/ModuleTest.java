60313f3f9c6156e69b92cfc2f009202969385adf
Shuaib <Zmac325@live.rhul.ac.uk>
2024-10-28 13:52:52 +0000

    Added ModuleTest Tests 4 and 5 for null/empty scenarios

--- CW1/src/test/java/uk/ac/rhul/cs2800/model/ModuleTest.java
+++ CW1/src/test/java/uk/ac/rhul/cs2800/model/ModuleTest.java
@@ -1,6 +1,9 @@
 package uk.ac.rhul.cs2800.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,10 +30,26 @@ class ModuleTest {
     // Test 3: Ensure that module details can be updated
     module.setCode("CS2900");
     module.setName("Advanced Software Engineering");
-    module.setMandatoryNonCondonable(false);   // add setters after 
+    module.setMandatoryNonCondonable(false); // add setters after
 
     assertEquals("CS2900", module.getCode());
     assertEquals("Advanced Software Engineering", module.getName());
-    assertFalse(module.isMandatoryNonCondonable()); // add is MNC after (returns mnc value) 
+    assertFalse(module.isMandatoryNonCondonable()); // add is MNC after (returns mnc value)
   }
 
+  @Test
+  void testInvalidModuleCode() {
+    // Test 4: Ensure that the module code cannot be null or empty
+    assertThrows(IllegalArgumentException.class,
+        () -> new Module("", "Software Engineering", true));
+    assertThrows(IllegalArgumentException.class,
+        () -> new Module(null, "Software Engineering", true));
+  }
+
+  @Test
+  void testInvalidModuleName() {
+    // Test 5: Ensure that the module name cannot be null or empty
+    assertThrows(IllegalArgumentException.class, () -> new Module("CS2800", "", true));
+    assertThrows(IllegalArgumentException.class, () -> new Module("CS2800", null, true));
+  }
+}

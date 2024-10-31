9caed14603c4d13cc8aea950dd28282f58145183
Shuaib <Zmac325@live.rhul.ac.uk>
2024-10-31 18:17:12 +0000

    Updated Grade to reference Module

--- CW1/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
+++ CW1/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
@@ -1,25 +1,19 @@
 package uk.ac.rhul.cs2800.model;
 
-/**
- * Represents a grade for a module in the student grade management system.
- */
 public class Grade {
   private int score;
+  private Module module; // Reference to the module
 
-  /**
-   * Constructs a new Grade with the specified score.
-   *
-   * @param score the grade score.
-   */
-  public Grade(int score) {
+  public Grade(int score, Module module) {
     this.score = score;
+    this.module = module;
   }
 
   public int getScore() {
     return score;
   }
 
-  public void setScore(int score) {
-    this.score = score;
+  public Module getModule() {
+    return module;
   }
 }

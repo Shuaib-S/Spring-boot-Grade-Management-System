3df43ef3d99c80e7fe2c60d2d41f9d75b9e225af
Shuaib <Zmac325@live.rhul.ac.uk>
2024-11-01 13:02:00 +0000

    Fixed summary javadoc is missing in NoRegistrationException

--- CW1/src/main/java/uk/ac/rhul/cs2800/exception/NoRegistrationException.java
+++ CW1/src/main/java/uk/ac/rhul/cs2800/exception/NoRegistrationException.java
@@ -1,19 +1,21 @@
 package uk.ac.rhul.cs2800.exception;
 
 /**
- * Exception thrown when a user tries to access grades for unregistered modules.
+ * Exception thrown when a user tries to access grades for an unregistered module.
+ * <p>
+ * This exception indicates that the user attempted to retrieve grades for a module that has not
+ * been registered in the system.
+ * </p>
  */
 public class NoRegistrationException extends Exception {
 
-  /**
-   * 
-   */
+  /** Serialization ID for maintaining version compatibility. */
   private static final long serialVersionUID = 6503724288101118059L;
 
   /**
    * Constructs a NoRegistrationException with a specified detail message.
    *
-   * @param message the detail message.
+   * @param message the detail message explaining the reason for the exception.
    */
   public NoRegistrationException(String message) {
     super(message);

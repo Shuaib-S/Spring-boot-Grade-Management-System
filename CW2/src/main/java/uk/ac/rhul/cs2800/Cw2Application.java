caac9d7aa1c27ad6f15ef86f89fb1dcb7d5a8b4f
Shuaib <Zmac325@live.rhul.ac.uk>
2024-11-14 14:33:47 +0000

    Modified CW2Application due to checkstlye issues after creating CI pipeline

--- CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/Cw2Application.java
@@ -3,11 +3,20 @@ package uk.ac.rhul.cs2800;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for CW2.
+ * This class initializes the Spring Boot application.
+ */
 @SpringBootApplication
 public class Cw2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cw2Application.class, args);
-	}
-
+  /**
+   * Main method to run the Spring Boot application.
+   *
+   * @param args command-line arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Cw2Application.class, args);
+  }
 }
+

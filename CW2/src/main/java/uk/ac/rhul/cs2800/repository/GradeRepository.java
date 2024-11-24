af46e05dbdc28c9be23938b26b26debcda11a781
Shuaib <Zmac325@live.rhul.ac.uk>
2024-11-24 17:36:04 +0000

    Added repository classes to extend Crud

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/repository/GradeRepository.java
@@ -0,0 +1,10 @@
+package uk.ac.rhul.cs2800.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.rhul.cs2800.model.Grade;
+
+/**
+ * Repository interface for Grade entity.
+ */
+public interface GradeRepository extends CrudRepository<Grade, Integer> {
+}

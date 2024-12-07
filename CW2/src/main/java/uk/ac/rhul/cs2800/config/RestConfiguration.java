530192607328cb93f623cd771dd1ad7b128f07e6
Shuaib <Zmac325@live.rhul.ac.uk>
2024-12-07 20:06:05 +0000

    added missing javadoc in RestConfiguration

--- CW2/src/main/java/uk/ac/rhul/cs2800/config/RestConfiguration.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/config/RestConfiguration.java
@@ -9,9 +9,18 @@ import uk.ac.rhul.cs2800.model.Grade;
 import uk.ac.rhul.cs2800.model.Module;
 import uk.ac.rhul.cs2800.model.Student;
 
+/**
+ * Configuration class for exposing entity IDs in the REST API.
+ */
 @Configuration
 public class RestConfiguration implements RepositoryRestConfigurer {
 
+  /**
+   * Configures the REST repository to expose entity IDs for specific classes.
+   *
+   * @param config the repository configuration to modify
+   * @param cors the CORS registry to configure
+   */
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
       CorsRegistry cors) {
@@ -19,5 +28,4 @@ public class RestConfiguration implements RepositoryRestConfigurer {
     config.exposeIdsFor(Module.class);
     config.exposeIdsFor(Grade.class);
   }
-
 }
